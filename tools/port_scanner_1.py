6be84046b239055661b089ec9e47ed0240c971d5
Hasan ali H <cyberhasan7@gmail.com>
2025-09-14 13:02:04 +0530

    Tool 1 - Commit #1

--- tools/port_scanner_1.py
+++ tools/port_scanner_1.py
@@ -1,5 +1,5 @@
 """
-Tool: port_scanner_1 | Commit #7
+Tool: port_scanner_1 | Commit #1
 Safe cybersecurity tool example: port_scanner
 """
 
@@ -16,4 +16,4 @@ def port_scanner(file_path="test.txt"):
 if __name__ == "__main__":
     print("port_scanner_1 output:", port_scanner())
 
-# Unique comment for commit #7
+# Unique comment for commit #1
